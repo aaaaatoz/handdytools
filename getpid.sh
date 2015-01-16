ed6fcc41bad2ef10ee38f99b865ba11f597ab01e
Rafa XU <rafa.xu.au@gmail.com>
2015-01-16 21:37:45 +1100

    Create getpid.sh

---
+++ getpid.sh
@@ -0,0 +1,9 @@
+#get the pid by User and Processname
+function GetPID #User #Name
+ {
+ PsUser=$1
+ PsName=$2
+ pid=`ps -u $PsUser|grep $PsName|grep -v grep|grep -v vi|grep -v dbx\n
+ |grep -v tail|grep -v start|grep -v stop |sed -n 1p |awk '{print $1}'`
+ echo $pid
+ }
