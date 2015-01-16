c04508803fb3175a0a1c29cbbf42562f2900647c
Rafa XU <rafa.xu.au@gmail.com>
2015-01-16 21:42:59 +1100

    Update getpid.sh

--- getpid.sh
+++ getpid.sh
@@ -3,7 +3,6 @@ function GetPID #User #Name
  {
  PsUser=$1
  PsName=$2
- pid=`ps -u $PsUser|grep $PsName|grep -v grep|grep -v vi|grep -v dbx\n
- |grep -v tail|grep -v start|grep -v stop |sed -n 1p |awk '{print $1}'`
+ pid=`ps -u $PsUser|grep $PsName|grep -v grep|grep -v vi|grep -v dbx\n |grep -v tail|grep -v start|grep -v stop |sed -n 1p |awk '{print $1}'`
  echo $pid
  }
